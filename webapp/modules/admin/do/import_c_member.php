66bf1aa1c2bd55e153deec8735c69bc6fc97df23
kunitsuji1109 <kunitsuji1109@b30932ad-a24a-4a87-96aa-b2bdcde4549c>
2008-10-09 11:55:42 +0000

    fix: #60 CSVインポート処理の関数名typo修正 (fixes #60)
    git-svn-id: http://usagi-project.org/svn/public/trunk@148 b30932ad-a24a-4a87-96aa-b2bdcde4549c

--- webapp/modules/admin/do/import_c_member.php
+++ webapp/modules/admin/do/import_c_member.php
@@ -149,7 +149,7 @@ class admin_do_import_c_member extends OpenPNE_Action
             //管理画面で指定したコミュニティに強制参加
             $c_commu_id_list = db_commu_regist_join_list();
             foreach ($c_commu_id_list as $c_commu_id) {
-                db_inc_join_c_commu($c_commu_id, $u);
+                do_inc_join_c_commu($c_commu_id, $u);
             }
 
             // --- データのインポート ここまで
