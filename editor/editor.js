a421d119294a51d42400085ee9f08680dcbad5f6
kunitsuji1109 <kunitsuji1109@b30932ad-a24a-4a87-96aa-b2bdcde4549c>
2008-11-28 11:45:03 +0000

    fix #7対応でエディターを変更したことによる不具合を修正 (refs #7)
    git-svn-id: http://usagi-project.org/svn/public/trunk@586 b30932ad-a24a-4a87-96aa-b2bdcde4549c

--- editor/editor.js
+++ editor/editor.js
@@ -1,12 +1,42 @@
+function checkVs(pn){
+    if (document.layers) {
+        if(document.layers[pn].display == 'none') {
+            return false;
+        } else {
+            return true;
+        }
+    } else if (document.all) {
+        if(document.all[pn].style.display == 'none') {
+            return false;
+        } else {
+            return true;
+        }
+    } else if (document.getElementById) {
+        if($(pn).style.display == 'none') {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
 function over(pct) {
-    document.getElementById(pct).className="over";
+    if(pct == 'change') {
+        document.getElementById(pct).className="overtext";
+    } else {
+        document.getElementById(pct).className="over";
+    }
 };
 
 function out(pct) {
-    if(flg != pct) {
-        document.getElementById(pct).className="flat";
+    if(pct == 'change') {
+        document.getElementById(pct).className="flattext";
     } else {
-        document.getElementById(pct).className="toggle";
+        if(flg != pct) {
+            document.getElementById(pct).className="flat";
+        } else {
+            document.getElementById(pct).className="toggle";
+        }
     }
 };
     
@@ -31,34 +61,59 @@ function showeditor(fnc,width,height) {
     }
 }
 
+function changeeditor() {
+    if(checkVs('bbcode_insert') && !checkVs('FeslyBBcode')) {
+        $('FeslyBBcode').style.display = 'block';
+        $('bbcode_insert').style.display = 'none';
+        document.cookie = "editor=1; expires=Thu, 1-Jan-2030 00:00:00 GMT";
+    } else {
+        $('FeslyBBcode').style.display = 'none';
+        $('bbcode_insert').style.display = 'block';
+        document.cookie = "editor=2; expires=Thu, 1-Jan-2030 00:00:00 GMT";
+    }
+}
+
 var area;
 
-function findarea() {
-     var e = null;
-    _txtarea = window.parent.document.getElementsByTagName("textarea");
-    for (i=0;i<_txtarea.length;i++) {
-        if (_txtarea.item(i).name.match(/^body|info|detail$/i)) {
-            e = _txtarea.item(i);
+Event.observe(window.document, 'mousedown', findarea, true);
+
+function findarea(e) {
+    var clk = Event.element(e);
+    if(clk.type == 'text' || clk.type == 'textarea') {
+        area = clk;
+    } else {
+        if(area == null || area.type != 'text') {
+            area = document.getElementsByName('body').item(0);
+        }
+    }
+} 
+
+document.write("<style>.flattext{margin:1px;padding:3px;border:#ffffff 1px solid;background-color:#ffffff;font-size:12px;color:#000000;}.overtext {margin:1px;padding:3px;border:#316AC5 1px solid;background-color:#DFF1FF;font-size:12px;color:#000000;text-decoration:none;}.flat{border:#ffffff 1px solid;background-color:#ffffff;}.over{border:#316AC5 1px solid;background-color:#DFF1FF;}.toggle{border:#316AC5 1px solid;background-color:#C1D2EE;}</style>");
+document.write("<div style=\"margin-top:3px;float:left;\"><img src=\"editor/auemoji/btn.gif\" id=\"auemoji\" name=\"btn\" alt=\"au絵文字挿入\" onClick=\"showeditor('auemoji',412,135);\" onmouseout=\"out('auemoji')\" onmouseover=\"over('auemoji')\" class=\"flat\" style=\"margin:0 1px;\"><img src=\"editor/dcmemoji/btn.gif\" id=\"dcmemoji\" name=\"btn\" alt=\"DoCoMo絵文字挿入\" onClick=\"showeditor('dcmemoji',286,188);\" onmouseout=\"out('dcmemoji')\" onmouseover=\"over('dcmemoji')\" class=\"flat\" style=\"margin:0 1px;\"><img src=\"editor/sbemoji/btn.gif\" id=\"sbemoji\" name=\"btn\" alt=\"SoftBank絵文字挿入\" onClick=\"showeditor('sbemoji',417,136);\" onmouseout=\"out('sbemoji')\" onmouseover=\"over('sbemoji')\" class=\"flat\" style=\"margin:0 1px;\"><img src=\"editor/kaomoji/btn.gif\" id=\"kaomoji\" name=\"btn\" alt=\"顔文字挿入\" onClick=\"showeditor('kaomoji');\" onmouseout=\"out('kaomoji')\" onmouseover=\"over('kaomoji')\" class=\"flat\" style=\"margin:0 1px;\"><img src=\"editor/map/btn.gif\" id=\"map\" name=\"btn\" alt=\"マップ挿入\" onClick=\"showeditor('map',400,485);\" onmouseout=\"out('map')\" onmouseover=\"over('map')\" class=\"flat\" style=\"margin:0 1px;\"></div>");
+
+if($('FeslyBBcode') && $('bbcode_insert')) {
+    document.write("<div style=\"margin-top:3px;padding:5px;float:right;\"><a href=\"javascript:void(0);\" onClick=\"changeeditor()\" onmouseout=\"out('change')\" onmouseover=\"over('change')\" class=\"flattext\" id=\"change\">BBcodeエディタ切替</a></div><br clear=\"both\">");
+    ckary = document.cookie.split("; ");
+    ckstr = 1;
+    for(i=0; i<ckary.length; i++) {
+        cid = ckary[i].split("=");
+        if (cid[0] == "editor") {
+            ckstr = cid[1];
             break;
         }
     }
-    return e;
-}
 
-Event.observe(window, 'load', loadEmojiArea, false);
-
-function loadEmojiArea() {
-    area = findarea();
-    if (typeof(area) != 'undefined' && !!area) {
-        var div = document.createElement('div');
-        div.setAttribute('style','margin-top:3px;');
-        div.innerHTML = "<img src=\"editor/auemoji/btn.gif\" id=\"auemoji\" name=\"btn\" alt=\"au絵文字挿入\" onClick=\"showeditor('auemoji',420,145);\" onmouseout=\"out('auemoji')\" onmouseover=\"over('auemoji')\" class=\"flat\" style=\"margin:0 1px;\"><img src=\"editor/dcmemoji/btn.gif\" id=\"dcmemoji\" name=\"btn\" alt=\"DoCoMo絵文字挿入\" onClick=\"showeditor('dcmemoji',295,200);\" onmouseout=\"out('dcmemoji')\" onmouseover=\"over('dcmemoji')\" class=\"flat\" style=\"margin:0 1px;\"><img src=\"editor/sbemoji/btn.gif\" id=\"sbemoji\" name=\"btn\" alt=\"SoftBank絵文字挿入\" onClick=\"showeditor('sbemoji',420,140);\" onmouseout=\"out('sbemoji')\" onmouseover=\"over('sbemoji')\" class=\"flat\" style=\"margin:0 1px;\"><img src=\"editor/kaomoji/btn.gif\" id=\"kaomoji\" name=\"btn\" alt=\"顔文字挿入\" onClick=\"showeditor('kaomoji');\" onmouseout=\"out('kaomoji')\" onmouseover=\"over('kaomoji')\" class=\"flat\" style=\"margin:0 1px;\"><img src=\"editor/map/btn.gif\" id=\"map\" name=\"btn\" alt=\"マップ挿入\" onClick=\"showeditor('map',420,485);\" onmouseout=\"out('map')\" onmouseover=\"over('map')\" class=\"flat\" style=\"margin:0 1px;\">";
-        var span = document.createElement('span');
-        span.setAttribute("id", "editor");
-        var pn = area.parentNode;
-        pn.appendChild(div);
-        pn.appendChild(span);
+    if(ckstr == 1) {
+        $('FeslyBBcode').style.display = 'block';
+        $('bbcode_insert').style.display = 'none';
+    } else {
+        $('FeslyBBcode').style.display = 'none';
+        $('bbcode_insert').style.display = 'block';
     }
+} else {
+    document.write("<br clear=\"both\">");
 }
 
-document.write("<style>.flat{border:#ffffff 1px solid;background-color:#ffffff;}.over{border:#316AC5 1px solid;background-color:#DFF1FF;}.toggle{border:#316AC5 1px solid;background-color:#C1D2EE;}</style>");
+document.write('<span id="editor"></span>');
+
+
