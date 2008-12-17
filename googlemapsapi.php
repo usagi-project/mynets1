c99e5973ca3f0b15cc7c07102b8011a1e887ce8d
kunitsuji1109 <kunitsuji1109@b30932ad-a24a-4a87-96aa-b2bdcde4549c>
2008-12-17 07:04:36 +0000

    fix #142、GoogleMapのAPIのバージョンを固定し、ソーシャルマップでエラーが出ないように変更 キシリスさん修正対応をマージ (refs #142)
    git-svn-id: http://usagi-project.org/svn/public/trunk@713 b30932ad-a24a-4a87-96aa-b2bdcde4549c

--- googlemapsapi.php
+++ googlemapsapi.php
@@ -3,9 +3,9 @@
  *
  * @license This source file is subject to version 3.01 of the PHP license,
  *              that is available at http://www.php.net/license/3_01.txt
- *              If you did not receive a copy of the PHP license and are unable 
- *              to obtain it through the world-wide-web, please send a note to 
- *              license@php.net so we can mail you a copy immediately.  
+ *              If you did not receive a copy of the PHP license and are unable
+ *              to obtain it through the world-wide-web, please send a note to
+ *              license@php.net so we can mail you a copy immediately.
  *
  * @category   Application of MyNETS
  * @project    OpenPNE UsagiProject 2006-2007
@@ -16,7 +16,8 @@
  * @version    MyNETS,v 1.0.0
  * @since      File available since Release 1.0.0 Nighty
  * @chengelog  [2007/02/17] Ver1.1.0Nighty package
- * ======================================================================== 
+ *             [2008/12/17] chg api version
+ * ========================================================================
  */
 
 
