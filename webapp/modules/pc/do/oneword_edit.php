159b9e70edd87ed1ee2846dee79f5faa7bc81773
kunitsuji1109 <kunitsuji1109@b30932ad-a24a-4a87-96aa-b2bdcde4549c>
2009-01-07 03:00:54 +0000

    fix #120 一言のXSｓ対策における正規表現に一部不具合があり、PHPのバージョンによってはエラーとなり動作しないバグを修正 (refs #120)
    git-svn-id: http://usagi-project.org/svn/public/branches/bugfix/1.2.0@761 b30932ad-a24a-4a87-96aa-b2bdcde4549c

--- webapp/modules/pc/do/oneword_edit.php
+++ webapp/modules/pc/do/oneword_edit.php
@@ -28,7 +28,7 @@ class pc_do_oneword_edit extends OpenPNE_Action
     {
         $uid     = $GLOBALS['AUTH']->uid();
         $id_to = $requests['id_to'];
-        $search = array('/<script[^>]*?>.*?</script>/si','/<[\/\!]*?[^<>]*?>/si');
+        $search = array('/<script[^>]*?>.*?<\/script>/si','/<[\/\!]*?[^<>]*?>/si');
         $replace = array('','');
         $oneword = preg_replace($search, $replace, $requests['value']);
         $moji_pattern = '/&(?:amp;|)#x([0-9A-F][0-9A-F][0-9A-F][0-9A-F]);/i';
@@ -43,16 +43,16 @@ class pc_do_oneword_edit extends OpenPNE_Action
         $word->setId_to($id_to);
         $word->set($oneword);
         $word->add();
-        
+
         $oneword = preg_replace_callback($moji_pattern, array($this, 'smarty_modifier_t_moji_callback'), $oneword);
-        
+
         if($oneword){
             echo $oneword;
         } else {
             echo "・・・・・・";
         }
     }
-    
+
     function smarty_modifier_t_moji_callback($matches) {
         $moji_file = sprintf('/moji/x_%s.gif',strtolower($matches[1]));
         if( is_readable("./img" . $moji_file) ) {
