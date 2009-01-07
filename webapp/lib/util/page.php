289404ebe294e7a76cdf7e37a3a0efea877706ec
kunitsuji1109 <kunitsuji1109@b30932ad-a24a-4a87-96aa-b2bdcde4549c>
2009-01-07 09:17:50 +0000

    fix #155 htmlspesialchars関数のラッパー関数記述漏れによる不具合 (fixes #155)
    git-svn-id: http://usagi-project.org/svn/public/branches/bugfix/1.2.0@767 b30932ad-a24a-4a87-96aa-b2bdcde4549c

--- webapp/lib/util/page.php
+++ webapp/lib/util/page.php
@@ -4,9 +4,9 @@
  *
  * @license This source file is subject to version 3.01 of the PHP license,
  *              that is available at http://www.php.net/license/3_01.txt
- *              If you did not receive a copy of the PHP license and are unable 
- *              to obtain it through the world-wide-web, please send a note to 
- *              license@php.net so we can mail you a copy immediately.  
+ *              If you did not receive a copy of the PHP license and are unable
+ *              to obtain it through the world-wide-web, please send a note to
+ *              license@php.net so we can mail you a copy immediately.
  *
  * @category   Application of MyNETS
  * @project    OpenPNE UsagiProject 2006-2007
@@ -16,7 +16,7 @@
  * @version    MyNETS,v 1.0.0
  * @since      File available since Release 1.0.0 Nighty
  * @chengelog  [2007/02/17] Ver1.1.0Nighty package
- * ======================================================================== 
+ * ========================================================================
  */
 
 /**
@@ -400,6 +400,47 @@ function message_body2inyou($string)
         $string = str_replace("\r\n", "\n", $string);
         $string = str_replace("\r", "\n", $string);
         $string = str_replace("\n", "\n> ", $string);
+    }
+    return $string;
+}
+/**
+ * HTML出力文字をエスケープする
+ * @param  string $string
+ * @return string エスケープされた$string
+ */
+function h($string)
+{
+    if (is_string($string))
+    {
+        $string = htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+    }
+    return $string;
+}
+
+/**
+ * HTML文字のエンティティ
+ * @param  string
+ * @return string HTMLエンティティ化された$string
+ */
+function ent_str($string)
+{
+    if (is_string($string))
+    {
+        $string = htmlentities($string, ENT_QUOTES, 'UTF-8');
+    }
+    return $string;
+}
+
+/**
+ * HTML文字のエンティティをデコード
+ * @param  string
+ * @return string HTMLエンティティをデコードした$string
+ */
+function dec_str($string)
+{
+    if (is_string($string))
+    {
+        $string = html_entity_decode($string, ENT_QUOTES, 'UTF-8');
     }
     return $string;
 }
