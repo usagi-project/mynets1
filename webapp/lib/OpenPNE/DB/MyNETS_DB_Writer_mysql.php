0422fccef56bc70a496ef4ad5d1b8e699659a446
kunitsuji1109 <kunitsuji1109@b30932ad-a24a-4a87-96aa-b2bdcde4549c>
2009-01-19 03:35:40 +0000

    fix #166 table prefixに-をつけたときのバグ回避 (fixes #166)
    git-svn-id: http://usagi-project.org/svn/public/branches/bugfix/1.2.0@831 b30932ad-a24a-4a87-96aa-b2bdcde4549c

--- webapp/lib/OpenPNE/DB/MyNETS_DB_Writer_mysql.php
+++ webapp/lib/OpenPNE/DB/MyNETS_DB_Writer_mysql.php
@@ -4,9 +4,9 @@
  *
  * @license This source file is subject to version 3.01 of the PHP license,
  *              that is available at http://www.php.net/license/3_01.txt
- *              If you did not receive a copy of the PHP license and are unable 
- *              to obtain it through the world-wide-web, please send a note to 
- *              license@php.net so we can mail you a copy immediately.  
+ *              If you did not receive a copy of the PHP license and are unable
+ *              to obtain it through the world-wide-web, please send a note to
+ *              license@php.net so we can mail you a copy immediately.
  *
  * @category   Application of MyNETS
  * @project    OpenPNE UsagiProject 2006-2007
@@ -16,7 +16,7 @@
  * @version    MyNETS,v 1.1.0
  * @since      File available since Release 1.1.0 Nighty
  * @chengelog  [2007/05/01] Ver1.1.0Nighty package
- * ======================================================================== 
+ * ========================================================================
  */
 
 
@@ -55,8 +55,8 @@ class MyNETS_DB_Writer extends MyNETS_DB
             $names .= $value;
             $values .= '?';
         }
-        $sql = "INSERT INTO $table ($names) VALUES ($values)";
-        
+        $sql = "INSERT INTO `$table` ($names) VALUES ($values)";
+
         if (!mysql_query($this->prepareSQL($sql, array_values($fields_values)), $this->db)) {
             return false;
         }
@@ -76,7 +76,7 @@ class MyNETS_DB_Writer extends MyNETS_DB
     function update($table, $fields_values, $where)
     {
         $where = $this->makeWhereClause($where);
-        
+
         $set = '';
         $first = true;
         $fields = array_keys($fields_values);
@@ -88,7 +88,7 @@ class MyNETS_DB_Writer extends MyNETS_DB
             }
             $set .= "$value = ?";
         }
-        $sql = "UPDATE $table SET $set";
+        $sql = "UPDATE `$table` SET $set";
         if ($where) {
             $sql .= " WHERE $where";
         }
