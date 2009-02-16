4904919fa6b879a02b02b05af2837685944012be
kunitsuji1109 <kunitsuji1109@b30932ad-a24a-4a87-96aa-b2bdcde4549c>
2009-02-16 02:43:45 +0000

    fix #188 トピックの作成権限がなくても作成出来てしまうバグを修正 (fixes #188)
    git-svn-id: http://usagi-project.org/svn/public/trunk@951 b30932ad-a24a-4a87-96aa-b2bdcde4549c

--- webapp/modules/ktai/page/c_home.php
+++ webapp/modules/ktai/page/c_home.php
@@ -4,9 +4,9 @@
  *
  * @license This source file is subject to version 3.01 of the PHP license,
  *              that is available at http://www.php.net/license/3_01.txt
- *              If you did not receive a copy of the PHP license and are unable 
- *              to obtain it through the world-wide-web, please send a note to 
- *              license@php.net so we can mail you a copy immediately.  
+ *              If you did not receive a copy of the PHP license and are unable
+ *              to obtain it through the world-wide-web, please send a note to
+ *              license@php.net so we can mail you a copy immediately.
  *
  * @category   Application of MyNETS
  * @project    OpenPNE UsagiProject 2006-2007
@@ -16,7 +16,7 @@
  * @version    MyNETS,v 1.0.0
  * @since      File available since Release 1.0.0 Nighty
  * @chengelog  [2007/02/17] Ver1.1.0Nighty package
- * ======================================================================== 
+ * ========================================================================
  */
 
 /**
@@ -79,6 +79,29 @@ class ktai_page_c_home extends OpenPNE_Action
 
         $this->set('is_unused_pc_bbs', util_is_unused_mail('m_pc_bbs_info'));
         $this->set('is_unused_ktai_bbs', util_is_unused_mail('m_ktai_bbs_info'));
+
+        //2009-02-16 KUNIHARU Tsujioka update
+        //トピックの作成権限が設定されていても、携帯では意識されていないので変更
+                //このコミュのトピック作成権限がどうなっているのか
+        $topic_admin = TRUE;
+        switch ($c_commu['topic_authority_role'])
+        {
+            case 1:             //指定のユーザーおよび管理者が作成可能
+                if ( _db_is_c_commu_topic_admin($c_commu_id, $u) < 1)
+                {
+                    $topic_admin = FALSE;
+                }
+                break;
+            case 99:            //管理者のみ作成可能
+                if ($c_commu['c_member_id_admin'] !== intval($u))
+                {
+                    $topic_admin = FALSE;
+                }
+                break;
+            default:            //メンバーであればだれでも作成可能
+                break;
+        }
+        $this->set('topic_admin', $topic_admin);
 
         return 'success';
     }
