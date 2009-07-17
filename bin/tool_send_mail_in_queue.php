3a59640848f751ce5600a1be1972d2776f18a05c
kunitsuji1109 <kunitsuji1109@b30932ad-a24a-4a87-96aa-b2bdcde4549c>
2009-07-17 09:18:56 +0000

    fix #244 Mail_Queue使用時のテーブル名にPREFIXがついてないために不具合が発生する (fixes #244)
    git-svn-id: http://usagi-project.org/svn/public/trunk@1109 b30932ad-a24a-4a87-96aa-b2bdcde4549c

--- bin/tool_send_mail_in_queue.php
+++ bin/tool_send_mail_in_queue.php
@@ -3,9 +3,9 @@
  *
  * @license This source file is subject to version 3.0 of the PHP license,
  *              that is available at http://www.php.net/license/3_01.txt
- *              If you did not receive a copy of the PHP license and are unable 
- *              to obtain it through the world-wide-web, please send a note to 
- *              license@php.net so we can mail you a copy immediately.  
+ *              If you did not receive a copy of the PHP license and are unable
+ *              to obtain it through the world-wide-web, please send a note to
+ *              license@php.net so we can mail you a copy immediately.
  *
  * @category   Application of MyNETS
  * @project    OpenPNE UsagiProject 2006-2007
@@ -15,7 +15,7 @@
  * @version    MyNETS,v 1.0.0
  * @since      File available since Release 1.0.0 Nighty
  * @chengelog  [2007/02/17] Ver1.1.0Nighty package
- * ======================================================================== 
+ * ========================================================================
  */
 
 /*
@@ -31,7 +31,7 @@ require_once 'Mail/Queue.php';
 $db_opt = array(
     "type"=>"db",
     "dsn"=>$GLOBALS['_OPENPNE_DSN_LIST']['main']['dsn'],
-    "mail_table"=>"mail_queue",
+    "mail_table"=>MYNETS_PREFIX_NAME."mail_queue",
 );
 $mail_opt = array(
     "driver"=>"mail",
